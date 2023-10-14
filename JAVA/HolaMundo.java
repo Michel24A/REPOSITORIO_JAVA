55812b85e0f19f29c8857cd1e443b4d2e3ec2499
Michel24A <136521044+Michel24A@users.noreply.github.com>
2023-10-14 13:37:31 -0600

    Add files via upload

---
+++ JAVA/HolaMundo.java
@@ -0,0 +1,7 @@
+/*Mi primer pograma */
+class HolaMundo {
+//Contenido dentro de la clase 
+public static void main (String [] args) {
+System.out.println("Hola Mundo"); 
+}
+}
---
+++ JAVA/HolaMundo/src/App.java
@@ -0,0 +1,17 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Integer a; 
+        int b;
+        a=10; 
+        b=20;
+        System.out.println(a+b);
+        
+        int caracterInt= Integer.parseInt("44");
+        String nombre= "Michel";
+        System.out.println("Hello, World!");
+        System.out.println(caracterInt);
+        System.out.println("Hola "+nombre);
+
+
+    }
+}
