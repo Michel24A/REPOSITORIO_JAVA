fc3362bfa407b719f112a4b753981012c2246ea7
Michel24A <136521044+Michel24A@users.noreply.github.com>
2023-10-28 13:31:18 -0600

    Add files via upload

---
+++ PRACTICA_2/src/Booleans.java
@@ -0,0 +1,7 @@
+public class Booleans {
+    public static void main(String[] args) throws Exception {
+        boolean intsPuedeAlmacenarDecimales=false;
+        System.out.println(intsPuedeAlmacenarDecimales);
+        
+    }
+}
